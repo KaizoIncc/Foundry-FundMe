1eafc041920b383a7905c740db5189441577b2ea
KaizoInc <kaizoinc22@gmail.com>
2025-04-21 11:56:27 +0200

    modules

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mock when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // If we are on local anvil chain, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if(block.chainid == 11155111) activeNetworkConfig = getSepoliaETHConfig();
+        else activeNetworkConfig = getOrCreateAnvilETHConfig();
+    }
+
+    function getSepoliaETHConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory SepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return SepoliaConfig;
+    }
+
+    function getOrCreateAnvilETHConfig() public returns (NetworkConfig memory) {
+        if(activeNetworkConfig.priceFeed != address(0)) return activeNetworkConfig;
+        // price feed address
+
+        // 1. Deploy mock
+        // 2. Return mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockAnvilPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockAnvilPriceFeed)});
+        return anvilConfig;
+    }
+}
---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+/**
+ * ¿Qué podemos hacer para trabajar con direcciones externas a nuestro sistema?
+ * - Unidad: Prueba de una parte específica de nuestro código
+ * - Integración: Prueba de cómo nuestro código funciona con otras partes
+ * - Bifurcación: Prueba de nuestro código en un entorno real simulado
+ * - Prueba de ensayo: Prueba de nuestro código en un entorno real no producido
+*/
+
+/**
+ * Para trabajar con tests debemos usar la metodologia 3A:
+ * - Arrange: configura el test
+ * - Act: realiza la accion que quieres testear
+ * - Assert: afirma el test
+ */
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint160 constant GAS_PRICE = 1;
+    
+    function setUp() external {
+        // yo -> FundMeTest -> FundMe = owner es FundMeTest, no yo.
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.getOwner(), msg.sender); // address(this) para comprobar que FundMeTest es el owner
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getaddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+    
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        // Act
+        // uint256 gasStart = gasleft(); // <- Cuanto gas queda en la llamada de transaccion
+        // vm.txGasPrice(GAS_PRICE);
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+        // uint256 gasEnd = gasleft(); // <- Cuanto gas hemos usado en la llamada de transaccion
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log(gasUsed);
+        // Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
+    }
+
+    function testWithdrawWithMultipleFunders() public funded {
+        // Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFundersIndex = 1; // No es 0 pork puede traer conflictos
+
+        for(uint160 i = startingFundersIndex; i < numberOfFunders; ++i) {
+            // vm.prank new address
+            // vm.deal new address
+            // hoax es una combinacion de ambas cosas
+            hoax(address(i), SEND_VALUE);
+            // fund the fundMe
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        // Act
+        // startPrank y stopPrank hacen en esencia lo mismo que prank pero con la address que tienen puesto
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+    }
+
+    function testWithdrawWithMultipleFundersCheaper() public funded {
+        // Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFundersIndex = 1; // No es 0 pork puede traer conflictos
+
+        for(uint160 i = startingFundersIndex; i < numberOfFunders; ++i) {
+            // vm.prank new address
+            // vm.deal new address
+            // hoax es una combinacion de ambas cosas
+            hoax(address(i), SEND_VALUE);
+            // fund the fundMe
+            fundMe.fund{value: SEND_VALUE}();
+        }
+        
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        // Act
+        // startPrank y stopPrank hacen en esencia lo mismo que prank pero con la address que tienen puesto
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
+    }
+}
